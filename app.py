e058cdac8025ce7b04a9ad36fba5e1c6d69c30cc
assoulyoscar-cloud <assouly.oscar@gmail.com>
2026-07-10 00:03:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_cors import CORS
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from config import Config
